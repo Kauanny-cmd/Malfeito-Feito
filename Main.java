95bd900fe4d948f6880c0fccee5939f00f8b9af8
Kauanny Vieira <72564756+Kauanny-cmd@users.noreply.github.com>
2021-06-28 20:11:12 +0000

    Organização de variáveis

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    String usuario = "Kauanny";
+    int tarefaHoje = 4;
+    int tarefaAmanha = 5;
+    int tarefaSemanal = tarefaHoje + tarefaAmanha;
+
+    boolean comTarefa = (tarefaSemanal > 0) ;
+    boolean semTarefa = (!comTarefa);
+
+    System.out.println("Olá " + usuario + "!");
+    System.out.println("Você tem um total de " + tarefaSemanal + " tarefas nessa semana");
+    System.out.println(comTarefa);
+    System.out.println(semTarefa);
   }
 }
