371e4e60ed52cc5860d0a51845e50c7249371013
Kauanny Vieira <72564756+Kauanny-cmd@users.noreply.github.com>
2021-07-05 18:33:39 +0000

    Organização simples das tarefas

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 class Main {
   public static void main(String[] args) {
+// Variantes de desenvolvimento
     String usuario = "Kauanny";
     int tarefaDomingo = 4;
     int tarefaSegunda = 5;
@@ -9,20 +10,27 @@ class Main {
     int tarefaSexta = 1;
     int tarefaSabado = 0;
 
+// Contagem de tarefas semanais
     int tarefaSemanal = tarefaDomingo + tarefaSegunda + tarefaTerca + tarefaQuarta + tarefaQuinta + tarefaSexta + tarefaSabado;
 
     boolean comTarefa = (tarefaSemanal > 0);
-    boolean semTarefa = (!comTarefa);
+  //boolean semTarefa = (!comTarefa);
 
-    System.out.println("Olá " + usuario + "!");
-    System.out.println("Você tem um total de " + tarefaSemanal + " tarefas nessa semana");
-    System.out.println("Veja a quantia em um único dia: ");
-    System.out.println("Domingo: " + tarefaDomingo);
-    System.out.println("Segunda: " + tarefaSegunda);
-    System.out.println("Terça: " + tarefaTerca);
-    System.out.println("Quarta: " + tarefaQuarta);
-    System.out.println("Quinta: " + tarefaQuinta);
-    System.out.println("Sexta: " + tarefaSexta);
-    System.out.println("Sábado: " + tarefaSabado);
+// Observando tarefas
+    if(comTarefa == true){
+      System.out.println("Olá " + usuario + "!");
+      System.out.println("Você tem um total de " + tarefaSemanal + " tarefas nesta semana!");
+      System.out.println("Veja a quantidade de tarefas ao longo da semana: ");
+      System.out.println("Domingo: " + tarefaDomingo);
+      System.out.println("Segunda: " + tarefaSegunda);
+      System.out.println("Terça: " + tarefaTerca);
+      System.out.println("Quarta: " + tarefaQuarta);
+      System.out.println("Quinta: " + tarefaQuinta);
+      System.out.println("Sexta: " + tarefaSexta);
+      System.out.println("Sábado: " + tarefaSabado); 
+    }else{
+      System.out.println("Olá " + usuario + "!");
+      System.out.println("Você não tem nenhuma tarefa essa semana!");
+    } 
   }
 }
