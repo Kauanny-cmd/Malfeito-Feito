c34b32c83fd86baf68da26c68b6bd3d4568f7c73
Kauanny <kauannyvieira8@gmail.com>
2021-08-03 16:25:49 -0300

    Classes de mostrar e add tarefas

--- Tarefas.java
+++ Tarefas.java
@@ -3,30 +3,37 @@ import java.util.ArrayList;
 
 public class Tarefas {
 
-    public static void BoasVindas() {
-        Scanner leitor = new Scanner(System.in);
-        String usuario;
+    private ArrayList<String> listaDeTarefas = new ArrayList<String>();
+    private int cont = 1;
 
-        System.out.println("Seja bem vindo ao Malfeito, feito!");
-        System.out.println("Por favor, digite o seu nome: ");
-        usuario = leitor.nextLine();
+
+    public static void BoasVindas(String usuario) {
         System.out.println("\nOlá " + usuario + "!\n");
     }
 
-    public static void AdicionaTarefas() {
+    public void AdicionaTarefas() {
         Scanner leitor = new Scanner(System.in);
-        int tarefaDomingo = 0;
+        int QuantidadeTarefa = 0;
 
-        ArrayList<String> listaDomingo = new ArrayList<String>();
-
-        System.out.println("Quantas tarefas você deseja adicionar para Domingo?");
-        tarefaDomingo = leitor.nextInt();
+        System.out.println("\n Quantas tarefas você deseja adicionar?");
+        QuantidadeTarefa = leitor.nextInt();
 
         System.out.println("Perfeito!");
         System.out.println("\nAgora, escreva, quais são elas:");
 
-        for (int i = 0; i <= tarefaDomingo; i++) {
-            listaDomingo.add(leitor.nextLine());
+        for (int i = 0; i <= QuantidadeTarefa; i++) {
+            listaDeTarefas.add(leitor.nextLine());
+        }
+    }
+
+    public void MostrarTarefas(String usuario) {
+        System.out.println("\n As tarefas são:");
+        if(listaDeTarefas.size() > 0){
+            for (String tarefa : listaDeTarefas) {
+                System.out.println((cont++) + "º " + tarefa + "\n");
+            }
+        }else if(listaDeTarefas.size() == 0){
+            System.out.println(usuario + ", você não tem nenhuma tarefa");
         }
     }
 }
