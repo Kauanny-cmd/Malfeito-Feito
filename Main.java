bf77571e4f0d486a3334bcaa5cbc58336a096f73
Kauanny <kauannyvieira8@gmail.com>
2021-08-18 16:00:37 -0300

    Mudança do final

--- Main.java
+++ Main.java
@@ -3,9 +3,8 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     // Variantes de desenvolvimento
-    Scanner leitor = new Scanner(System.in);
+    final Scanner leitor = new Scanner(System.in);
 
-    // String MaisTarefa = "sim";
     boolean verTarefas = true;
 
     // Boas vindas
@@ -42,68 +41,5 @@ public class Main {
       }
       
     }
-    
-
-    // Contagem de tarefas semanais
-     /* int tarefaSemanal = tarefaDomingo + tarefaSegunda + tarefaTerca + tarefaQuarta + tarefaQuinta + tarefaSexta + tarefaSabado;
-
-    boolean comTarefa = (tarefaSemanal > 0); */ 
-    // boolean semTarefa = (!comTarefa);
-
-    // Observando tarefas
-    // if(comTarefa == true){
-    // System.out.println("Você tem um total de " + tarefaSemanal + " tarefas nesta
-    // semana!");
-    // System.out.println("Veja a quantidade de tarefas ao longo da semana: ");
-    // System.out.println("Domingo: " + tarefaDomingo);
-    // System.out.println("Segunda: " + tarefaSegunda);
-    // System.out.println("Terça: " + tarefaTerca);
-    // System.out.println("Quarta: " + tarefaQuarta);
-    // System.out.println("Quinta: " + tarefaQuinta);
-    // System.out.println("Sexta: " + tarefaSexta);
-    // System.out.println("Sábado: " + tarefaSabado);
-    // }else{
-    // System.out.println("Olá " + usuario + "!");
-    // System.out.println("Você não tem nenhuma tarefa essa semana!");
-    // }
-
-    // // Pergunta sobre a opção de mais tarefas
-    // do{
-    // System.out.println("Vamos adicionar mais tarefas?");
-    // switch(MaisTarefa){
-    // case "sim": System.out.println("Vamos lá então");
-    // break;
-    // case "Sim": System.out.println("Vamos lá então");
-    // break;
-    // case "s": System.out.println("Vamos lá então");
-    // break;
-    // case "não": System.out.println("Ok, sem mais tarefas");
-    // break;
-    // case "Não": System.out.println("Ok, sem mais tarefas");
-    // break;
-    // case "nao": System.out.println("Ok, sem mais tarefas");
-    // break;
-    // case "n": System.out.println("Ok, sem mais tarefas");
-    // break;
-    // default: System.out.println("Responda com 'sim' ou 'não'");
-    // tarefas--;
-    // }
-    // }
-    // while(tarefas==0);
-
-    // Semana
-    // int DiasSemana = 0;
-    // int dia = 0;
-    // Exemplo com while
-    // while(DiasSemana < 7){
-    // dia++;
-    // DiasSemana++;
-    // }
-    // Exemplo com for
-    // for(dia = 0; DiasSemana < 7; dia++){
-    // DiasSemana++;
-    // }
-    // System.out.println(dia);
-    // System.out.println(DiasSemana);
   }
 }
