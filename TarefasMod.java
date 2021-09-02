ea9a1f691c2531c399e9974e5ff1f47c26432f32
Kauanny <kauannyvieira8@gmail.com>
2021-09-02 14:18:38 -0300

    Adicionando interface

---
+++ TarefasMod.java
@@ -0,0 +1,3 @@
+public interface TarefasMod {
+    public final int i = 0;
+}
--- User.java
+++ User.java
@@ -1,4 +1,4 @@
-public abstract class User {
+public abstract class User implements TarefasMod {
     public String usuario;
 
     public void User(){
